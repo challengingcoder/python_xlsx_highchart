05c01e3bf02d5603b808e6e1de86c5e821ba0bd1
excellentprogrammer <newrealtopten@gmail.com>
2018-03-04 23:18:07 +0100

    use tls instead of ssl

--- src/pptxbuilder/app.py
+++ src/pptxbuilder/app.py
@@ -56,11 +56,11 @@ def __run_server():
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['MAIL_SERVER']='smtp.gmail.com'
-    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_PORT'] = 587
     app.config['MAIL_USERNAME'] = email
     app.config['MAIL_PASSWORD'] = password
-    app.config['MAIL_USE_TLS'] = False
-    app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False
     app.config['MAIL_DEBUG'] = True
     app.run(host='0.0.0.0', port=5000, threaded=True)
 
